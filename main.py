b7ce1f5c25190a225174913cfe980deaf8ee8225
Ivo <ivailo.yordano.ph@gmail.com>
2023-06-21 23:34:34 +0300

    from laptop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import re
 
@@ -16,7 +17,7 @@ class Warehouse:
         self.SEPARATOR_WHEN_PRINTING = " | "
         self.file_name = file_name
         if not file_name:
-            file_name = "app_data.csv"
+            self.file_name = "app_data.csv"
         self.LETTER_SECTION = "ab"
         self.NUM_SECTION = 2
         self.NUM_SHELVES = 3
@@ -46,10 +47,10 @@ class Warehouse:
                                 ]
 
     def run_app(self):
-        print(c.get_yellow("y"))
-        print(c.get_bold_yellow("yb"))
-        for i in self.special_options:
-            print(i)
+        self.create_db_file_if_missing()
+
+        # for i in self.special_options:
+        #     print(i)
 
     def print_csv_formatted(self, db_list: list):
         pass
@@ -102,10 +103,85 @@ class Warehouse:
         print()
         self.print_menu_options_in_frame(title, self.special_options, c.ANSI_GREEN)
 
+    def create_db_file_if_missing(self) -> None:
+        if not os.path.exists(self.file_name):
+            open(self.file_name, 'w').close()
+
+    def get_db_to_list(self) -> list[list[str]]:
+        """
+        Reads the DB and adds every element in a row to list.
+
+        :return:
+        [['Coca Cola', '02.03.2020', '03.04.2022', 'The Coca-Cola Company', 'unit', '200',
+        'b1 / 1 / 3', '1000', 'original taste'], [..], [..]]
+        """
+        db_file = []
+        with open(self.file_name, 'r') as db:
+            rows = db.readlines()
+            for row in rows:
+                inner_list = [el.rstrip() for el in row.split(self.SEPARATOR)[:-1]]
+                db_file.append(inner_list)
+
+        return db_file
+
+    def get_db_to_list_with_description(self) -> list[list[str]]:
+        """
+        Read the DB and adds description to each field
+
+        :return:
+        [['Name: Coca Cola', 'Expiry date: 02.03.2020', 'Entry date: 03.04.2022',
+        'Manufacturer: The Coca-Cola Company', 'Unit: unit', 'Stock: 200', 'Position: b1 / 1 / 3',
+        'Available items at shelf: 1000', 'Comment: original taste'], [..], [..]]
+        """
+        db_file = self.get_db_to_list()
+        db_with_desc = []
+        for inner_list in db_file:
+            inner_with_desc = []
+            for i, item in enumerate(inner_list):
+                if i == 0:
+                    description = "Name"
+                else:
+                    description = self.DESCRIPTION_NO_NAME[i - 1]
+                inner_with_desc.append(f"{description}: {item}")
+            db_with_desc.append(inner_with_desc)
+
+        return db_with_desc
+
+    def write_list_to_db(self, list_to_write: list) -> bool:
+        string_build = self.SEPARATOR.join(list_to_write)
+        try:
+            with open(self.file_name, 'a') as db:
+                db.write(string_build)
+            return True
+        except Exception as e:
+            c.print_error(f"There was a problem writing \"{list_to_write}\" to the file: {e}")
+            return False
+
+    # todo
+    def write_all_data_to_db(self):
+        print("Product was added successfully!")
+
+    def is_at_least_one_item_in_location(self, location: str) -> bool:
+        db = self.get_db_to_list()
+        for row in db:
+            if row[6] == location:
+                return True
+        return False
+
+    def get_all_unique_locations_for_item(self, item_name: str, expiry_date: str):
+        db = self.get_db_to_list()
+        set_of_positions = {, }
+        for row in db:
+            if item_name == row[0] and expiry_date == row[1]:
+                set_of_positions.add(row[6])
+
+
 
 if __name__ == "__main__":
     wh = Warehouse()
+    # wh = Warehouse('t.csv')
     # wh.run_app()
-    wh.get_all_valid_user_input()
-
-
+    # result = wh.get_db_to_list()
+    # print(result)
+    print(wh.is_at_least_one_item_in_location('b3 / 1 / 3'))
+    print(wh.is_at_least_one_item_in_location('g3 / 1 / 3'))
